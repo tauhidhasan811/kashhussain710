9c8ea70b6b1db87d6cab6e66896ee106ec0617c1
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-28 16:57:29 +0600

    create car prompt and add fastapi

--- component/asset/prompt_tempt.py
+++ component/asset/prompt_tempt.py
@@ -3,17 +3,23 @@ from langchain_core.prompts import PromptTemplate
 
 def GenPrompt(mot_info, user_query, previous_chat):
     sys_message = SystemMessage(
-        content="You are a chat assignment"
-                "Your task is answer user query based on " \
-                "the cars  Ministry of Transport (MTO) report data"
+        content=(
+            "You are a chat assistant. "
+            "Your task is to answer the user query based on "
+            "the car Ministry of Transport (MOT) report data."
+        )
     )
 
     human_message = HumanMessage(
-        content=f"Current user query: {user_query} \n\MOT data: {mot_info}\nUser previous query: {previous_chat}"
+        content=(
+            f"Current user query: {user_query}\n"
+            f"MOT data: {mot_info}\n"
+            f"User previous query: {previous_chat}"
+        )
     )
 
     temp = PromptTemplate(
-        "System Message: {sys_message} \n {human_message}",
+        template="System Message: {sys_message}\n\nHuman Message: {human_message}",
         input_variables=['sys_message', 'human_message']
     )
 
--- main.py
+++ main.py
@@ -1,38 +1,11 @@
-# import requests
+from component.asset.prompt_tempt import GenMTOPrompt, GenMotorPrompt
+from fastapi import FastAPI
 
-# url = "https://kashhussain710-backend-nestjs.onrender.com/api/v1/check-car/free"
+app = FastAPI()
 
-# headers = {
-#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5YmNkMGJhYWI3YzhjODljNmU1OGRjNSIsImVtYWlsIjoidXNlckBnbWFpbC5jb20iLCJyb2xlIjoidXNlciIsImlhdCI6MTc3NDY2OTEzOCwiZXhwIjoxNzc1MjczOTM4fQ.rXZiDwJPA0iUBtIjM1I_hVpjNRkmdlU0kG81jcSGP2s"
-# }
+@app.post
+async def Analysis
 
-# data = {
-#     "registrationNumber": "AB12CDE"
-# }
+prompt = GenMTOPrompt("MOT information", "User Query", "Previous chat")
 
-# response = requests.post(url, headers=headers, json=data)
-
-# print("Status Code:", response.status_code)
-# print("Response:", response.text)
-
-import requests
-import json
-url = "https://history.mot.api.gov.uk/v1/trade/vehicles"
-
-
-
-headers = {
-    "Accept": "application/json+v6",
-    "x-api-key": "B5fnZJm6F7axEdOZXaqGc8a5m6X6UqGT7yfRAU1B"
-}
-
-data = {
-    "registrationNumber": "AB12CDE"
-}
-
-response = requests.post(url, headers=headers, json=data)
-print(response)
-with open('mot_data.json', 'w', encoding='utf-8') as f:
-    json.dump(response.json(), f, indent=4)
-print("Status Code:", response.status_code)
-print("Response:", response.text)
+print(prompt)
