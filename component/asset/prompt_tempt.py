cc1d0ada6985da55f6636c9320e2108dec94a32a
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-29 11:35:19 +0600

    add fastapi and chat model

--- component/asset/prompt_tempt.py
+++ component/asset/prompt_tempt.py
@@ -1,25 +1,43 @@
 from langchain.messages import SystemMessage, HumanMessage
 from langchain_core.prompts import PromptTemplate
 
-def GenPrompt(mot_info, user_query, previous_chat):
+def GenMTOPrompt(mot_info, user_query, previous_chat):
     sys_message = SystemMessage(
-        content=(
-            "You are a chat assistant. "
-            "Your task is to answer the user query based on "
-            "the car Ministry of Transport (MOT) report data."
-        )
+        content=("You are a chat assignment"
+                "Your task is answer user query based on " 
+                "the cars  Ministry of Transport (MTO) report data")
     )
 
     human_message = HumanMessage(
-        content=(
-            f"Current user query: {user_query}\n"
-            f"MOT data: {mot_info}\n"
-            f"User previous query: {previous_chat}"
-        )
+        content=f"Current user query: {user_query} \nMOT data: {mot_info}\nUser previous query: {previous_chat}"
     )
 
     temp = PromptTemplate(
-        template="System Message: {sys_message}\n\nHuman Message: {human_message}",
+        template="System Message: {sys_message} \n {human_message}",
+        input_variables=['sys_message', 'human_message']
+    )
+
+    prompt = temp.invoke(
+        input={
+            'sys_message': sys_message.content,
+            'human_message': human_message.content
+        }
+    )
+
+    return prompt
+
+def GenMotorPrompt(motor_info, user_query, previous_chat):
+    sys_message = SystemMessage(
+        content=("You are a chat assignment"
+                "Your task is answer user query based on the Cars/Motor report data")
+    )
+
+    human_message = HumanMessage(
+        content=f"Current user query: {user_query} \ncar data: {motor_info}\nUser previous query: {previous_chat}"
+    )
+
+    temp = PromptTemplate(
+        template="System Message: {sys_message} \n {human_message}",
         input_variables=['sys_message', 'human_message']
     )
 
--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from component.asset.prompt_tempt import GenMTOPrompt, GenMotorPrompt
+from component.asset.prompt_tempt import  GenMotorPrompt, GenMTOPrompt
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from component.config.openai_model import LoadGPT
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+llm = LoadGPT()
 
-@app.post
-async def Analysis
+@app.post("/analysis/motor")
+async def AnalysisCarData(motor_info: str, user_query: str, previous_chat: str):
+    prompt = GenMotorPrompt(motor_info=motor_info,
+                          user_query=user_query,
+                          previous_chat=previous_chat)
+    
+    response = llm.invoke(prompt.text)
+    return JSONResponse(
+        status_code=200,
+        content={
+            'status': True,
+            'message': response.content
+        }
+    )
 
-prompt = GenMTOPrompt("MOT information", "User Query", "Previous chat")
+# prompt = GenMTOPrompt("MOT information", "User Query", "Previous chat")
 
-print(prompt)
+# print(prompt.text)
